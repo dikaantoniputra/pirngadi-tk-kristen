bf186a04c19945721ad0c9ed12397c5ce43a6e5b
dikaantoniputra <dikahanggara2@gmail.com>
2024-10-09 17:35:42 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('landing.page.maintaice');
+});
+
+Route::get('/index', function () {
     return view('landing.page.index');
 });
 
